74f844c3b0cd2ffab094fbaf9fc6781dc4ae3ede
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-03-02 21:53:29 +0100

    tuple.py

---
+++ tuple.py
@@ -0,0 +1,8 @@
+# Create a new tuple and print it. 
+# 1. Create a new tuple that includes the names of four students. 
+# 2. Print the tuple to the console so each data type will appear in a separate line. 
+
+
+lista = ("Monika","Slawek","Marek")
+typ = type(lista)
+print(f'Lista studentów {lista} \n {typ}')
